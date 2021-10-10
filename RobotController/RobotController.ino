fb2a901488c39c3bef2118cb71c553a8f7b6e19f
JW2190 <77811954+JW2190@users.noreply.github.com>
2021-10-10 12:56:18 +0100

    Created RobotController Script. Started adafruit motor controlling

---
+++ RobotController/RobotController.ino
@@ -0,0 +1,18 @@
+//inital important notes:
+//when doing follow the line code, do not update motor speeds to same value as they currently were. can strain the network
+//https://wokwi.com/arduino/new?template=arduino-uno
+
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_MS_PWMServoDriver.h"
+
+
+void setup() {
+  // put your setup code here, to run once:
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
