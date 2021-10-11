6735c0c6bce2fa3889dc6c9cd0dc509b9f05ae76
JW2190 <77811954+JW2190@users.noreply.github.com>
2021-10-11 16:49:42 +0100

    started line following test code

--- test_script/test_script.ino
+++ test_script/test_script.ino
@@ -2,6 +2,8 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600); //Start serial and set the correct Baud Rate
   Serial.print("Test");
+  pinMode(4,OUTPUT);
+  pinMode(A5,INPUT);
 }
 
 void loop() {
