ac6358755cf5d4c06d0cd370695199a55d9e9da9
JW2190 <77811954+JW2190@users.noreply.github.com>
2021-10-15 14:05:39 +0100

    3 sensor line following script with prep for digital line sensor input

--- Line_Following/Line_Following.ino
+++ Line_Following/Line_Following.ino
@@ -17,21 +17,24 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 Adafruit_DCMotor *right_motor = AFMS.getMotor(1);
 Adafruit_DCMotor *left_motor = AFMS.getMotor(2);
 
-
-//Sensor Connection
-const int sensor_pins [2] = {A0, A1};
-
-
 // Sensor Values Global
+const int NUM_OF_SENSORS = 3;
 float BLACK_TO_WHITE_MULTIPLIER = 0.80;
-int SENSOR_THRESHOLD_L;
-int SENSOR_THRESHOLD_R;
-int sensor_state [2];
+int SENSOR_THRESHOLDS[NUM_OF_SENSORS]; //
+int SENSOR_THRESHOLD [NUM_OF_SENSORS];
+int sensor_state [NUM_OF_SENSORS];
+int sensor_state_bin [NUM_OF_SENSORS];
 int sensor_stateR;
 int sensor_stateL;
+int sensor_stateM;
+
+//Sensor Connection
+const int sensor_pins [NUM_OF_SENSORS] = {A0, A1, A2}; //{L,,M,R}
+
 
 // Motor Speeds Global
 const int HIGH_MOTOR_SPEED = 255;
+const int MID_MOTOR_SPEED = 200;
 const int LOW_MOTOR_SPEED = 150;
 
 //turn delay
@@ -40,11 +43,16 @@ const int turn_delay = 50;
 //run 1 execution code in loop() (needed for slightly later start of motors)
 bool first = true;
 
+//PID objects
+float P ,I ,D ,PIDvalue, previousError,Kp, Ki, Kd;
+int error = 0;
+
 //Modes
 enum mode_frame{LEFT=0,RIGHT=1,STRAIGHT=2,STOP=3};
 mode_frame mode = STRAIGHT;
+
 //DEBUG: Toggle functionality easily:
-bool MotorsOn = true;
+bool MotorsOn = false;
 bool PRINT_SENSOR_STATES = false;
 bool PRINT_TURNING_DECISIONS = true;
 
@@ -67,27 +75,34 @@ void setup() {
   //AUTO CALIBRATE SENSORS (Must be started on BLACK surface)
     delay(100);
     Serial.println("--------CALIBRATION-------");
-    
-    sensor_stateL = analogRead(sensor_pins[0]);
-    sensor_stateR = analogRead(sensor_pins[1]);
-    
+    for(byte x=0;x<NUM_OF_SENSORS;x++){
+        sensor_state[x] = analogRead(sensor_pins[x]);;
+        }
+        
     //Getting average of 10 sensor readings and multiplying by scale factor to guess white threshold
     for(byte i=0;i<10;i++){
-      sensor_stateL = (sensor_stateL+analogRead(sensor_pins[0]))/2;
-      sensor_stateR = (sensor_stateR+analogRead(sensor_pins[1]))/2;
-      Serial.print("Left Sensor Black AVG: ");
-      Serial.print(sensor_stateL);
-      Serial.print("     Right Sensor Black AVG: "); 
-      Serial.println(sensor_stateR);
+      for(byte x=0;x<NUM_OF_SENSORS;x++){
+        sensor_state[x] = (sensor_state[x]+analogRead(sensor_pins[x]))/2;
+        }
+      Serial.print("Left Black AVG: ");
+      Serial.print(sensor_state[0]);
+      Serial.print("     Mid Black AVG: "); 
+      Serial.println(sensor_state[1]);
+      Serial.print("     Right Black AVG: "); 
+      Serial.println(sensor_state[2]);
       delay(100);
     }
-    SENSOR_THRESHOLD_L = BLACK_TO_WHITE_MULTIPLIER * sensor_stateL;
-    SENSOR_THRESHOLD_R = BLACK_TO_WHITE_MULTIPLIER * sensor_stateR;
+    for(byte x=0;x<NUM_OF_SENSORS;x++){
+      SENSOR_THRESHOLD[x] = BLACK_TO_WHITE_MULTIPLIER * sensor_state[x];
+      
+      }
     
     Serial.print("Left Sensor Threshold: ");
-    Serial.println(SENSOR_THRESHOLD_L);
+    Serial.println(SENSOR_THRESHOLD[0]);
+    Serial.print("Mid Sensor Threshold: "); 
+    Serial.println(SENSOR_THRESHOLD[1]);
     Serial.print("Right Sensor Threshold: "); 
-    Serial.println(SENSOR_THRESHOLD_R);
+    Serial.println(SENSOR_THRESHOLD[2]);
     Serial.println("--------------------------");
     
 }
@@ -97,8 +112,9 @@ void setup() {
 void loop() {
 
   //READ SENSOR STATES
-  sensor_stateL = analogRead(sensor_pins[0]);
-  sensor_stateR = analogRead(sensor_pins[1]);
+  for(byte x=0;x<NUM_OF_SENSORS;x++){
+        sensor_state[x] = analogRead(sensor_pins[x]);;
+        }
   if(PRINT_SENSOR_STATES){
     Serial.print("L: ");
     Serial.print(sensor_stateL);
@@ -131,6 +147,7 @@ void loop() {
     right_motor->run(RELEASE);
     }
 
+  analoguetobin();
   calc_mode();
   follow_line();
   delay(turn_delay);
@@ -147,8 +164,7 @@ void setmotorspeed(int L_speed,int R_speed){
 
 //--------------------------------------------------------------------------------
 
-//PID function and objects:
-float P ,I ,D ,PIDvalue, error, previousError,Kp, Ki, Kd;
+//PID function
 void calculatePID(){
   P = error;
   I = I + error;
@@ -161,22 +177,26 @@ void calculatePID(){
 //--------------------------------------------------------------------------------
 
 void follow_line(){
-  switch(mode){
-    case LEFT:
-    if(PRINT_TURNING_DECISIONS){Serial.println("turning left");}
+  switch(error){
+    case 2:
+    if(PRINT_TURNING_DECISIONS){Serial.println("sharp left");}
     setmotorspeed(LOW_MOTOR_SPEED,HIGH_MOTOR_SPEED);
     break;
-    case RIGHT:
-    if(PRINT_TURNING_DECISIONS){Serial.println("turning right");}
-    setmotorspeed(HIGH_MOTOR_SPEED,LOW_MOTOR_SPEED);
+    case 1:
+    if(PRINT_TURNING_DECISIONS){Serial.println("slight left");}
+    setmotorspeed(MID_MOTOR_SPEED,HIGH_MOTOR_SPEED);
     break;
-    case STRAIGHT:
-    if(PRINT_TURNING_DECISIONS){Serial.println("going forward");}
+    case 0:
+    if(PRINT_TURNING_DECISIONS){Serial.println("forward");}
     setmotorspeed(HIGH_MOTOR_SPEED,HIGH_MOTOR_SPEED);
     break;
-    case STOP:
-    if(PRINT_TURNING_DECISIONS){Serial.println("stop");}
-    setmotorspeed(0,0);
+    case -1:
+    if(PRINT_TURNING_DECISIONS){Serial.println("slight right");}
+    setmotorspeed(HIGH_MOTOR_SPEED,MID_MOTOR_SPEED);
+    break;
+    case -2:
+    if(PRINT_TURNING_DECISIONS){Serial.println("sharp right");}
+    setmotorspeed(HIGH_MOTOR_SPEED,LOW_MOTOR_SPEED);
     break;
   }
 }
@@ -184,8 +204,19 @@ void follow_line(){
 
 //--------------------------------------------------------------------------------
 
+//1 0 0   2
+//1 1 0   1
+//0 1 0   0
+//0 1 1   -1
+//0 0 1   -2
+
+//0 0 0
+//1 1 1
+
+
 //Contains turn delay
 void calc_mode(){
+  /*
   //If RIGHT SENSOR LOW (WHITE) and LEFT SENSOR LOW (WHITE) STOP
   if(sensor_stateR < SENSOR_THRESHOLD_R && sensor_stateL < SENSOR_THRESHOLD_L){mode = STOP;return;}
   //If RIGHT SENSOR LOW (WHITE) and LEFT SENSOR HIGH (BLACK) turn RIGHT
@@ -194,11 +225,34 @@ void calc_mode(){
   else if(sensor_stateR > SENSOR_THRESHOLD_R && sensor_stateL > SENSOR_THRESHOLD_L){mode = STRAIGHT;return;}
   //If RIGHT SENSOR HIGH (BLACK) and LEFT SENSOR LOW (WHITE) turn LEFT
   else if(sensor_stateR < SENSOR_THRESHOLD_R && sensor_stateL > SENSOR_THRESHOLD_L){mode = LEFT;return;}
-  
+  */
+  //1 0 0   2 sharp left
+  if(sensor_state_bin[0]==1 && sensor_state_bin[1]==0 && sensor_state_bin[2]==0){mode = LEFT;error = 2;return;}
+  //1 1 0   1 small left
+  else if(sensor_state_bin[0]==1 && sensor_state_bin[1]==1 && sensor_state_bin[2]==0){mode = LEFT;error = 1;return;}
+  //0 1 0   0 straight
+  else if(sensor_state_bin[0]==0 && sensor_state_bin[1]==1 && sensor_state_bin[2]==0){mode = STRAIGHT;error = 0;return;}
+  //0 1 1   -1 small right
+  else if(sensor_state_bin[0]==0 && sensor_state_bin[1]==1 && sensor_state_bin[2]==1){mode = RIGHT;error = -1;return;}
+  //0 0 1   -2 sharp right
+  else if(sensor_state_bin[0]==0 && sensor_state_bin[1]==0 && sensor_state_bin[2]==1){mode = RIGHT;error = -2;return;}
 }
 
 //--------------------------------------------------------------------------------
 
+//temporary script until alex finishes sensor circuit
 void analoguetobin(){
-  
+  for(byte x=0;x<NUM_OF_SENSORS;x++){
+    if(sensor_state[x] < SENSOR_THRESHOLD[x]){
+      sensor_state_bin[x] = 0;
+    }
+    else{
+      sensor_state_bin[x] = 1;
+      }
+      //Serial.println(sensor_state_bin[x]);
+    }
+    Serial.print(sensor_state_bin[0]);
+    Serial.print(sensor_state_bin[1]);
+    Serial.println(sensor_state_bin[2]);
+    
   }
